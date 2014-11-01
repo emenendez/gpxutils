3e6c665e154464013123e91f814642e9448e52b8
Eric Menendez <ericmenendez@gmail.com>
2014-11-01 09:37:54 -0400

    Version 1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='gpxutils',
 
-    version='1.0.1',
+    version='1.0.2',
 
     description='Tools for working with GPX files',
     long_description=long_description,
